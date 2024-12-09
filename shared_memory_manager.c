4e3627a82a042765033bea88a720898accbd4dc7
Esraa hassan <136431551+esraahassanasu@users.noreply.github.com>
2024-12-09 18:45:07 +0200

    sfree not correct aah

---
+++ shared_memory_manager.c
@@ -0,0 +1,324 @@
+#include <inc/memlayout.h>
+#include "shared_memory_manager.h"
+
+#include <inc/mmu.h>
+#include <inc/error.h>
+#include <inc/string.h>
+#include <inc/assert.h>
+#include <inc/queue.h>
+#include <inc/environment_definitions.h>
+
+#include <kern/proc/user_environment.h>
+#include <kern/trap/syscall.h>
+#include "kheap.h"
+#include "memory_manager.h"
+
+//==================================================================================//
+//============================== GIVEN FUNCTIONS ===================================//
+//==================================================================================//
+struct Share* get_share(int32 ownerID, char* name);
+
+//===========================
+// [1] INITIALIZE SHARES:
+//===========================
+//Initialize the list and the corresponding lock
+void sharing_init()
+{
+#if USE_KHEAP
+	LIST_INIT(&AllShares.shares_list);
+	init_spinlock(&AllShares.shareslock, "shares lock");
+#else
+	panic("not handled when KERN HEAP is disabled");
+#endif
+}
+
+//==============================
+// [2] Get Size of Share Object:
+//==============================
+int getSizeOfSharedObject(int32 ownerID, char* shareName)
+{
+	//[PROJECT'24.MS2] DONE
+	// This function should return the size of the given shared object
+	// RETURN:
+	//	a) If found, return size of shared object
+	//	b) Else, return E_SHARED_MEM_NOT_EXISTS
+	//
+	struct Share* ptr_share = get_share(ownerID, shareName);
+	if (ptr_share == NULL)
+		return E_SHARED_MEM_NOT_EXISTS;
+	else
+		return ptr_share->size;
+
+	return 0;
+}
+
+//===========================================================
+
+
+//==================================================================================//
+//============================ REQUIRED FUNCTIONS ==================================//
+//==================================================================================//
+//===========================
+// [1] Create frames_storage:
+//===========================
+// Create the frames_storage and initialize it by 0
+// Create the frames_storage and initialize it by 0
+inline struct FrameInfo** create_frames_storage(int numOfFrames)
+{
+	//TODO: [PROJECT'24.MS2 - #16] [4] SHARED MEMORY - create_frames_storage()
+	//COMMENT THE FOLLOWING LINE BEFORE START CODING
+	//panic("create_frames_storage is not implemented yet");
+	//Your Code is Here...
+
+	if( numOfFrames <= 0){
+		return NULL;
+	}
+	//cprintf("\n---->start calling kmalloc in create frame storage\n");
+	acquire_spinlock(&MemFrameLists.mfllock);
+	struct FrameInfo** storage_array = kmalloc(numOfFrames*sizeof(struct  FrameInfo* ));
+	release_spinlock(&MemFrameLists.mfllock);
+
+	    if (storage_array == NULL) {
+	        return NULL;
+	    }
+
+	   // cprintf("\n---->end calling kmalloc in create frame storage\n");
+
+
+	for(int i = 0 ; i < numOfFrames ; i++){
+			storage_array[i] = NULL;
+		}
+
+    return  storage_array;
+}
+//=====================================
+// [2] Alloc & Initialize Share Object:
+//=====================================
+//Allocates a new shared object and initialize its member
+//It dynamically creates the "framesStorage"
+//Return: allocatedObject (pointer to struct Share) passed by reference
+struct Share* create_share(int32 ownerID, char* shareName, uint32 size, uint8 isWritable)
+{
+	//TODO: [PROJECT'24.MS2 - #16] [4] SHARED MEMORY - create_share()
+	//COMMENT THE FOLLOWING LINE BEFORE START CODING
+	//panic("create_share is not implemented yet");
+	//Your Code is Here...
+	//cprintf("\n---->start calling kmalloc in create share\n");
+	acquire_spinlock(&MemFrameLists.mfllock);
+	struct Share* sharedObj = kmalloc(sizeof(struct Share));
+	release_spinlock(&MemFrameLists.mfllock);
+	uint32 num_pages = ROUNDUP(size, PAGE_SIZE) / PAGE_SIZE;
+	//cprintf("\n---->end calling kmalloc in create share\n");
+
+
+	if (sharedObj == NULL) {
+		return NULL;
+	}
+
+	sharedObj->n_frames=num_pages;
+	sharedObj->ownerID = ownerID;
+	sharedObj->size = size;
+	sharedObj->isWritable = isWritable;
+	sharedObj->references = 1;
+	sharedObj->ID = ((int32)sharedObj & 0x7FFFFFFF);
+
+	strcpy(sharedObj->name, shareName);
+	sharedObj->framesStorage = create_frames_storage(num_pages);
+
+		if (sharedObj->framesStorage  == NULL) {
+//			acquire_spinlock(&MemFrameLists.mfllock);
+			kfree(sharedObj->framesStorage );
+//			release_spinlock(&MemFrameLists.mfllock);
+			return NULL;
+		}
+	return sharedObj;
+}//=============================
+// [3] Search for Share Object:
+//=============================
+//Search for the given shared object in the "shares_list"
+//Return:
+//	a) if found: ptr to Share object
+//	b) else: NULL
+struct Share* get_share(int32 ownerID, char* name)
+{
+	//TODO: [PROJECT'24.MS2 - #17] [4] SHARED MEMORY - get_share()
+		//COMMENT THE FOLLOWING LINE BEFORE START CODING
+		//panic("get_share is not implemented yet");
+		//Your Code is Here...
+    struct Share* current_share;
+
+    //cprintf("Searching for shared object with name: %s, ownerID: %d\n", name, ownerID);
+    acquire_spinlock(&AllShares.shareslock);
+
+    LIST_FOREACH(current_share, &AllShares.shares_list) {
+       // cprintf("Checking object: %s, ownerID: %d\n", current_share->name, current_share->ownerID);
+
+        if (current_share->ownerID == ownerID && strcmp(current_share->name, name) == 0) {
+           // cprintf("Found shared object: %s, ownerID: %d\n", current_share->name, current_share->ownerID);
+            release_spinlock(&AllShares.shareslock);
+            return current_share; // Found the shared object
+        }
+    }
+
+    //cprintf("Shared object not found.\n");
+    release_spinlock(&AllShares.shareslock);
+    return NULL; // Not found
+}
+//=========================
+// [4] Create Share Object:
+//=========================
+int createSharedObject(int32 ownerID, char* shareName, uint32 size, uint8 isWritable, void* virtual_address)
+{
+	//TODO: [PROJECT'24.MS2 - #19] [4] SHARED MEMORY [KERNEL SIDE] - createSharedObject()
+	//COMMENT THE FOLLOWING LINE BEFORE START CODING
+	//panic("createSharedObject is not implemented yet");
+	//Your Code is Here...
+		if(get_share(ownerID, shareName)!=NULL){
+		return E_SHARED_MEM_EXISTS;
+	}
+
+	struct Share* newShare = create_share(ownerID,shareName,size,isWritable);
+
+	    if (newShare == NULL)
+	    {
+	        return E_NO_SHARE;
+	    }
+		struct Env* myenv = get_cpu_proc();
+
+	    uint32 n_pages=ROUNDUP(size,PAGE_SIZE)/PAGE_SIZE;
+
+		for (uint32 i =0 ; i< n_pages;i++)
+	    {
+			uint32 va=(uint32)virtual_address+i*PAGE_SIZE;
+			struct FrameInfo * frame=NULL;
+				    allocate_frame(& frame);
+				    map_frame(myenv->env_page_directory,frame,va,PERM_WRITEABLE | PERM_PRESENT | PERM_USER | PERM_MARKED);
+
+				    newShare->framesStorage[i]=frame;
+	    }
+
+
+	    acquire_spinlock(&AllShares.shareslock);
+	    LIST_INSERT_TAIL(&AllShares.shares_list, newShare);
+	    release_spinlock(&AllShares.shareslock);
+
+	    return newShare->ID;
+
+}
+//======================
+// [5] Get Share Object:
+//======================
+int getSharedObject(int32 ownerID, char* shareName, void* virtual_address)
+{
+	//TODO: [PROJECT'24.MS2 - #21] [4] SHARED MEMORY [KERNEL SIDE] - getSharedObject()
+	//COMMENT THE FOLLOWING LINE BEFORE START CODING
+	//panic("getSharedObject is not implemented yet");
+	//Your Code is Here...
+
+	struct Env* myenv = get_cpu_proc(); //The calling environment
+	    if (myenv == NULL)
+	    {
+	        return E_SHARED_MEM_NOT_EXISTS;
+	    }
+	    struct Share* share = get_share(ownerID, shareName);
+
+	    acquire_spinlock(&AllShares.shareslock);
+	    uint32 nsize=ROUNDUP(share->size,PAGE_SIZE)/PAGE_SIZE;
+	       for (uint32 i = 0; i < nsize; i++)
+	          {
+
+	    	  // cprintf("da5l elfor loop \n");
+
+	              uint32* va = virtual_address +( i * PAGE_SIZE);
+
+	             // cprintf("da5el y map  \n");
+	              uint32 perms = PERM_USER;
+	                    if (share->isWritable) {
+	                        perms |= PERM_WRITEABLE;
+	                    }
+	                    map_frame(myenv->env_page_directory, share->framesStorage[i], (uint32)va, perms);
+
+	             // cprintf("mapmazbota \n");
+	             // cprintf("i = %d ,n_frames= %d \n",i ,share->n_frames);
+	          }
+	       	   release_spinlock(&AllShares.shareslock);
+
+	          share->references++;
+
+	          return share->ID;
+}
+
+//==================================================================================//
+//============================== BONUS FUNCTIONS ===================================//
+//==================================================================================//
+
+//==========================
+// [B1] Delete Share Object:
+//==========================
+//delete the given shared object from the "shares_list"
+//it should free its framesStorage and the share object itself
+void free_share(struct Share* ptrShare)
+{
+	//TODO: [PROJECT'24.MS2 - BONUS#4] [4] SHARED MEMORY [KERNEL SIDE] - free_share()
+	//COMMENT THE FOLLOWING LINE BEFORE START CODING
+	//panic("free_share is not implemented yet");
+	//Your Code is Here...
+	acquire_spinlock(&AllShares.shareslock);
+	LIST_REMOVE(&AllShares.shares_list, ptrShare);
+	release_spinlock(&AllShares.shareslock);
+	for (uint32 i =0 ; i < ptrShare->n_frames ; i++ )
+	{
+		free_frame(ptrShare->framesStorage[i]);
+	}
+	kfree((void*)ptrShare);
+}
+//========================
+// [B2] Free Share Object:
+//========================
+int freeSharedObject(int32 sharedObjectID, void *startVA)
+{
+	//TODO: [PROJECT'24.MS2 - BONUS#4] [4] SHARED MEMORY [KERNEL SIDE] - freeSharedObject()
+	//COMMENT THE FOLLOWING LINE BEFORE START CODING
+	//panic("freeSharedObject is not implemented yet");
+	//Your Code is Here...
+	struct Env* myenv = get_cpu_proc(); //The calling environment
+		    if (myenv == NULL)
+		    {
+		        return E_SHARED_MEM_NOT_EXISTS;
+		    }
+	int found = 0 ;
+	 struct Share* current_share;
+	 uint32 va = (uint32)startVA;
+	acquire_spinlock(&AllShares.shareslock);
+
+	    LIST_FOREACH(current_share, &AllShares.shares_list) {
+
+	        if (current_share->ID == sharedObjectID) {
+	        	found = 1 ;
+	            break;
+	        }
+	    }
+         release_spinlock(&AllShares.shareslock);
+         if (found == 0 )
+         {
+        	 cprintf("not found the id \n");
+        	 return E_SHARED_MEM_NOT_EXISTS;
+         }
+         for (uint32 i =0 ; i < current_share->n_frames ; i++ )
+         {
+        	 unmap_frame(myenv->env_page_directory,va);
+        	 if( pd_is_table_used(myenv->env_page_directory,va)== 0 )
+        	        		{
+        	        	pd_clear_page_dir_entry(myenv->env_page_directory,va);
+        	        		}
+
+        	 va = va + PAGE_SIZE;
+         }
+        current_share->references -- ;
+        if (current_share->references == 0 )
+        {
+        	free_share(current_share);
+        }
+        tlbflush();
+return 0 ;
+}
---
+++ uheap.c
@@ -0,0 +1,281 @@
+
+#include <inc/lib.h>
+#define total ((USER_HEAP_MAX - USER_HEAP_START) / PAGE_SIZE) + 100
+//==================================================================================//
+//============================ REQUIRED FUNCTIONS ==================================//
+//==================================================================================//
+
+//=============================================
+// [1] CHANGE THE BREAK LIMIT OF THE USER HEAP:
+//=============================================
+/*2023*/
+void* sbrk(int increment) {
+	return (void*) sys_sbrk(increment);
+}
+
+//=================================
+// [2] ALLOCATE SPACE IN USER HEAP:
+//=================================
+
+bool marked[total], is_start[total];
+
+void* malloc(uint32 size) {
+	if (size == 0)
+			return NULL;
+		if (size <= DYN_ALLOC_MAX_BLOCK_SIZE)
+			return alloc_block_FF(size);
+
+		if (sys_isUHeapPlacementStrategyFIRSTFIT()) {
+			size = ROUNDUP(size, PAGE_SIZE);
+			uint32 needed_pages = size / PAGE_SIZE;
+
+			uint32 count = 0, va;
+			uint32 start_page = myEnv->u_limit + PAGE_SIZE;
+			for (uint32 i = start_page; i < USER_HEAP_MAX; i += PAGE_SIZE) {
+				uint32 x = (i - start_page) / PAGE_SIZE;
+				if (marked[x]) {
+					count = 0;
+				} else {
+					if (!count)
+						va = i;
+					count++;
+				}
+				if (count >= needed_pages)
+					break;
+			}
+
+			if (count >= needed_pages) {
+				uint32 y = (va - start_page) / PAGE_SIZE;
+				is_start[y] = 1;
+
+				for (uint32 i = y; i < y + needed_pages; i++) {
+					marked[i] = 1;
+				}
+				sys_allocate_user_mem(va, size);
+				return (void *) va;
+
+			}
+		}
+
+		// No suitable block found
+		return NULL;
+}
+
+
+//=================================
+// [3] FREE SPACE FROM USER HEAP:
+//=================================
+void free(void* virtual_address) {
+	//TODO: [PROJECT'24.MS2 - #14] [3] USER HEAP [USER SIDE] - free()
+	// Write your code here, remove the panic and write your code
+	uint32 va = (uint32) virtual_address;
+	uint32 start_page = myEnv->u_limit + PAGE_SIZE;
+	if (va >= USER_HEAP_START && va < myEnv->u_limit)
+		free_block(virtual_address);
+	else if (va >= start_page && va < USER_HEAP_MAX) {
+
+		uint32 y = (va - start_page) / PAGE_SIZE,numofpages=0;
+		if (is_start[y]) {
+			is_start[y]=0;
+			while(marked[y]&&!is_start[y])
+			{
+				numofpages++;
+				marked[y]=0;
+				y++;
+			}
+
+			sys_free_user_mem(va,numofpages*PAGE_SIZE);
+
+		}
+
+
+	} else
+		panic("Invalid virtual address");
+
+	return;
+
+}
+
+//=================================
+// [4] ALLOCATE SHARED VARIABLE:
+//=================================
+
+void* smalloc(char *sharedVarName, uint32 size, uint8 isWritable)
+{
+	//==============================================================
+	//DON'T CHANGE THIS CODE========================================
+	//==============================================================
+	//TODO: [PROJECT'24.MS2 - #18] [4] SHARED MEMORY [USER SIDE] - smalloc()
+	// Write your code here, remove the panic and write your code
+//	panic("smalloc() is not implemented yet...!!");
+	if (size == 0) return NULL ;
+
+	if(size<PAGE_SIZE){
+		size=PAGE_SIZE;
+	}
+			size = ROUNDUP(size, PAGE_SIZE);
+			uint32 needed_pages = size / PAGE_SIZE;
+
+			uint32 count = 0, va;
+			uint32 start_page = myEnv->u_limit + PAGE_SIZE;
+			for (uint32 i = start_page; i < USER_HEAP_MAX; i += PAGE_SIZE) {
+				uint32 x = (i - start_page) / PAGE_SIZE;
+				if (marked[x]) {
+					count = 0;
+				} else {
+					if (!count)
+						va = i;
+					count++;
+				}
+				if (count >= needed_pages)
+					break;
+			}
+
+			if (count >= needed_pages) {
+				uint32 y = (va - start_page) / PAGE_SIZE;
+				is_start[y] = 1;
+
+				for (uint32 i = y; i < y + needed_pages; i++) {
+					marked[i] = 1;
+				}
+				//(char* shareName, uint32 size, uint8 isWritable,void* virtual_address)
+				int x = sys_createSharedObject(sharedVarName, size,isWritable,(void*)va);
+				if (x ==E_NO_SHARE || x==E_SHARED_MEM_EXISTS )
+				{
+					return NULL;
+				}
+				return (void *) va;
+			}
+
+	return NULL;
+}
+
+//========================================
+// [5] SHARE ON ALLOCATED SHARED VARIABLE:
+//========================================
+void* sget(int32 ownerEnvID, char *sharedVarName)
+{
+	//TODO: [PROJECT'24.MS2 - #20] [4] SHARED MEMORY [USER SIDE] - sget()
+	// Write your code here, remove the panic and write your code
+//	panic("sget() is not implemented yet...!!");
+
+	int size = sys_getSizeOfSharedObject(ownerEnvID,
+					sharedVarName);
+		if (size <= 0||size == E_SHARED_MEM_NOT_EXISTS)
+			return NULL;
+
+		size = ROUNDUP(size, PAGE_SIZE);
+			uint32 needed_pages = size / PAGE_SIZE;
+
+			uint32 seq = 0, count = 0, va;
+			uint32 start_page = myEnv->u_limit + PAGE_SIZE;
+			for (uint32 i = start_page; i < USER_HEAP_MAX; i += PAGE_SIZE) {
+				uint32 x = (i - start_page) / PAGE_SIZE;
+				if (marked[x]) {
+					count = 0;
+				} else {
+					if (!count)
+						va = i;
+					count++;
+				}
+				if (count >= needed_pages)
+					break;
+			}
+
+			if (count >= needed_pages) {
+				uint32 y = (va - start_page) / PAGE_SIZE;
+				is_start[y] = 1;
+				marked[y] = 1;
+
+				for (uint32 i = y; i < y + needed_pages; i++) {
+					marked[i] = 1;
+				}
+
+				int x=sys_getSharedObject(ownerEnvID,sharedVarName,(void*)va);
+				if (x==E_SHARED_MEM_NOT_EXISTS )
+				{
+					return NULL;
+				}
+				else
+				{
+					return (void *)va;
+				}
+
+			}
+
+		return NULL;
+}
+
+
+//==================================================================================//
+//============================== BONUS FUNCTIONS ===================================//
+//==================================================================================//
+
+//=================================
+// FREE SHARED VARIABLE:
+//=================================
+//	This function frees the shared variable at the given virtual_address
+//	To do this, we need to switch to the kernel, free the pages AND "EMPTY" PAGE TABLES
+//	from main memory then switch back to the user again.
+//
+//	use sys_freeSharedObject(...); which switches to the kernel mode,
+//	calls freeSharedObject(...) in "shared_memory_manager.c", then switch back to the user mode here
+//	the freeSharedObject() function is empty, make sure to implement it.
+
+void sfree(void* virtual_address) {
+//TODO: [PROJECT'24.MS2 - BONUS#4] [4] SHARED MEMORY [USER SIDE] - sfree()
+// Write your code here, remove the panic and write your code
+	//panic("sfree() is not implemented yet...!!");
+	int32 va = (int32)virtual_address;
+	/*if ((uint32)virtual_address%PAGE_SIZE!=0)
+	{
+		 va= ROUNDDOWN((int32)virtual_address,PAGE_SIZE);
+	}*/
+	if (va <= KERNEL_HEAP_START || va > KERNEL_HEAP_MAX)
+	{
+		cprintf("not in the kernel memo\n");
+	}
+	int32 masked= (va &0x7FFFFFFF);
+	sys_freeSharedObject(masked,(void*)va);
+
+}
+
+//=================================
+// REALLOC USER SPACE:
+//=================================
+//	Attempts to resize the allocated space at "virtual_address" to "new_size" bytes,
+//	possibly moving it in the heap.
+//	If successful, returns the new virtual_address, in which case the old virtual_address must no longer be accessed.
+//	On failure, returns a null pointer, and the old virtual_address remains valid.
+
+//	A call with virtual_address = null is equivalent to malloc().
+//	A call with new_size = zero is equivalent to free().
+
+//  Hint: you may need to use the sys_move_user_mem(...)
+//		which switches to the kernel mode, calls move_user_mem(...)
+//		in "kern/mem/chunk_operations.c", then switch back to the user mode here
+//	the move_user_mem() function is empty, make sure to implement it.
+void *realloc(void *virtual_address, uint32 new_size) {
+//[PROJECT]
+// Write your code here, remove the panic and write your code
+	panic("realloc() is not implemented yet...!!");
+	return NULL;
+
+}
+
+//==================================================================================//
+//========================== MODIFICATION FUNCTIONS ================================//
+//==================================================================================//
+
+void expand(uint32 newSize) {
+	panic("Not Implemented");
+
+}
+void shrink(uint32 newSize) {
+	panic("Not Implemented");
+
+}
+void freeHeap(void* virtual_address) {
+	panic("Not Implemented");
+
+}
