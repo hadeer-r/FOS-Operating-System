abc35be21d26d310c3bc258457aa24851690b9b5
Esraa hassan <136431551+esraahassanasu@users.noreply.github.com>
2024-12-20 10:19:07 +0200

    Update shared_memory_manager.h

--- kern/mem/shared_memory_manager.h
+++ kern/mem/shared_memory_manager.h
@@ -10,7 +10,11 @@
 //#include <inc/memlayout.h>
 #include <inc/environment_definitions.h>
 #include <kern/conc/spinlock.h>
-
+struct idref
+{
+	int32 id ;
+	uint32 refid ;
+};
 struct Share
 {
 	//Unique ID for this Share object
@@ -27,7 +31,7 @@ struct Share
 	uint32 references;
 	//sharing permissions (0: ReadOnly, 1:Writable)
 	uint8 isWritable;
-
+uint32  freeva[100];
 	//to store frames to be shared
 	struct FrameInfo** framesStorage;
 	uint32 n_frames;
@@ -36,27 +40,31 @@ struct Share
 
 };
 
+
 //List of all shared objects
 LIST_HEAD(Share_List, Share);		// Declares 'struct Share_List'
-
+//LIST_HEAD(id_List, idforfree);
 #if USE_KHEAP == 0
-	//max number of shared objects
-	#define MAX_SHARES 100
-	struct Share shares[MAX_SHARES] ;
+    // Max number of shared objects when using static memory allocation
+    #define MAX_SHARES 100
+    struct Share shares[MAX_SHARES];
+   // struct idforfree ids[MAX_SHARES];
 #else
-	struct
-	{
-		struct Share_List shares_list ;	//List of all share variables created by any process
-		struct spinlock shareslock;		//Use it to protect the shares_list in the kernel
-	}AllShares;
+    struct
+    {
+        struct Share_List shares_list; // List of all share objects created by any process
+        struct spinlock shareslock;    // Spinlock to protect the shares_list in the kernel
+    } AllShares;
 
-	void sharing_init();
+
+    void sharing_init();
 #endif
+
 
 int createSharedObject(int32 ownerID, char* shareName, uint32 size, uint8 isWritable, void* virtual_address);
 int getSizeOfSharedObject(int32 ownerID, char* shareName);
 int getSharedObject(int32 ownerID, char* shareName, void* virtual_address);
 int freeSharedObject(int32 sharedObjectID, void *startVA);
-
+int32 getsharedid(void* virtual_adress);
 
 #endif /* FOS_SHARED_MEMORY_MANAGER_H */
