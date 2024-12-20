ec23398e9ae01e0931c76edc0d85a4e2c93e2e24
Esraa hassan <136431551+esraahassanasu@users.noreply.github.com>
2024-12-20 10:28:28 +0200

    Update lib.h for shared

--- inc/lib.h
+++ inc/lib.h
@@ -102,6 +102,7 @@ int 	sys_createSharedObject(char* shareName, uint32 size, uint8 isWritable, void
 int 	sys_getSizeOfSharedObject(int32 ownerID, char* shareName);
 int 	sys_getSharedObject(int32 ownerID, char* shareName, void* virtual_address );
 int 	sys_freeSharedObject(int32 sharedObjectID, void *startVA);
+int32      sys_get_shared_id(void* virtual_adress);
 
 //etc...
 uint32	sys_rcr2();
